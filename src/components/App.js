113748bf8954b7ff6fde3e7c98b26eb56262524a
UEHARA Junji <uehaj@jggug.org>
2016-07-11 06:04:59 +0900

    init

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+// app/components/App.jsx
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <h1>Hello, world!</h1>
+    );
+  }
+}
