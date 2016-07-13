8b79fa6fc80a81173089e6a4f5c87f352cfb5b62
UEHARA Junji <uehaj@jggug.org>
2016-07-13 09:23:32 +0900

    remove blanks before newline

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
 // http://minotaur.badwitch.io/react-webpack-boilerplate/
-var path = require('path');  
-var HtmlwebpackPlugin = require('html-webpack-plugin');  
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 var merge = require('webpack-merge');
 
 const TARGET = process.env.npm_lifecycle_event;
-process.env.BABEL_ENV = TARGET;  // トップレベルに追加  
+process.env.BABEL_ENV = TARGET;  // トップレベルに追加
 
-const PATHS = {  
+const PATHS = {
   app:   path.join(__dirname, 'src'),
   build: path.join(__dirname, 'build')
 };
 
 // 共通設定
-const common = {  
+const common = {
   entry: PATHS.app,
 
   resolve: {
     // import/requireをするときに拡張子を省略できるようにする
     extensions: ['', '.js', '.jsx']
   },
-  
+
   output: {
     path: PATHS.build,
     filename: 'build.js'
@@ -46,7 +46,7 @@ const common = {
 };
 
 // npm startを実行した時の設定
-if(TARGET === 'start' || !TARGET) {  
+if(TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
     devtool: 'eval-source-map',
     devServer: {
@@ -66,7 +66,7 @@ if(TARGET === 'start' || !TARGET) {
 }
 
 // npm buildを実行した時の設定
-if(TARGET === 'build') {  
+if(TARGET === 'build') {
   module.exports = merge(common, {
   });
 }
