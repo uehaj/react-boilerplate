7ba3e145b0f002f63e74ee5f75ad49326bcd0168
UEHARA Junji <uehaj@jggug.org>
2016-07-16 18:57:56 +0900

    gh-pages initial

--- src/actions.js
+++
@@ -1,7 +0,0 @@
-import { createAction } from "redux-actions"
-import * as types from "./types"
-
-// redux-actionsは、Flux Standard Action(FSA)規約に従う
-// Actionを作るためのライブラリ。createActionがそのような
-// 標準Actionを作ってくれる関数。
-export const inputDataChange = createAction(types.INPUT_DATA_CHANGE, (obj) => obj)
