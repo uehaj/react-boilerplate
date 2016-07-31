dd332fab22dde953a0ccf94585c306e6ee09f1c8
@uehaj <uehaj@jggug.org>
2016-07-31 18:44:33 +0900

    remove debug log and comment

--- src/container.js
+++ src/container.js
@@ -7,12 +7,10 @@ import DevTools from './containers/DevTools';
 import * as actions from "./actions"
 
 function mapStateToProps(state){
-  console.log(state)
   return { bmiValue: state.bmiCalc.bmiValue }
 }
 
 function mapDispatchToProps(dispatch) {
-  // dispatchとかcomponent以下に渡したくないのでここでbindしてしまう。
   return bindActionCreators(actions, dispatch)
 }
 
