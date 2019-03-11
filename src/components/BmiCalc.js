73544322f93311d298b7f8ce2858479b3a938f22
UEHARA, Junji <59012+uehaj@users.noreply.github.com>
2019-03-11 23:49:29 +0900

    change class to functional the BmiCalc component

--- src/components/BmiCalc.js
+++ src/components/BmiCalc.js
@@ -1,49 +1,50 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
 import NumberField from './NumberField';
 
-export default class BmiCalc extends React.Component {
-  handleChange() {
-    const height = parseInt(this.height.value);
-    const weight = parseInt(this.weight.value);
-    this.props.onChange({ height, weight });
-  }
+export default function BmiCalc(props) {
+  const inputHeight = useRef(null);
+  const inputWeight = useRef(null);
 
-  render() {
-    return (
-      <>
-        <AppBar position="static">BMI</AppBar>
-        <Grid>
-          <Row style={{ padding: '1em' }}>
-            <Col>
-              <NumberField
-                inputRef={el => (this.height = el)}
-                label="身長を入力してください"
-                onChange={this.handleChange.bind(this)}
-              />
-            </Col>
-            <Col>
-              <NumberField
-                inputRef={el => (this.weight = el)}
-                label="体重を入力してください"
-                onChange={el => this.handleChange(el)}
-              />
-            </Col>
-          </Row>
-          <Row style={{ padding: '1em' }}>
-            {this.props.bmiValue ? (
-              <h1>あなたのBMIは{this.props.bmiValue}です</h1>
-            ) : (
-              <h1>身長と体重を入力してください</h1>
-            )}
-          </Row>
-        </Grid>
-      </>
-    );
-  }
+  const handleChange = () => {
+    const height = parseInt(inputHeight.current.value);
+    const weight = parseInt(inputWeight.current.value);
+    props.onChange({ height, weight });
+  };
+
+  return (
+    <>
+      <AppBar position="static">BMI</AppBar>
+      <Grid>
+        <Row style={{ padding: '1em' }}>
+          <Col>
+            <NumberField
+              inputRef={inputHeight}
+              label="身長を入力してください"
+              onChange={handleChange}
+            />
+          </Col>
+          <Col>
+            <NumberField
+              inputRef={inputWeight}
+              label="体重を入力してください"
+              onChange={handleChange}
+            />
+          </Col>
+        </Row>
+        <Row style={{ padding: '1em' }}>
+          {props.bmiValue ? (
+            <h1>あなたのBMIは{props.bmiValue}です</h1>
+          ) : (
+            <h1>身長と体重を入力してください</h1>
+          )}
+        </Row>
+      </Grid>
+    </>
+  );
 }
 
 BmiCalc.propTypes = {
