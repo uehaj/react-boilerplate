32174f7e2ee57f024a9b4f4d813a8bf4bb8a5157
UEHARA, Junji <59012+uehaj@users.noreply.github.com>
2019-03-11 23:18:19 +0900

    remove devtools stuff

--- src/container.js
+++ src/container.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import { bindActionCreators } from "redux"
-import { connect, Provider } from "react-redux"
-import App from "./components/App"
-import DevTools from './containers/DevTools';
+import { bindActionCreators } from 'redux';
+import { connect, Provider } from 'react-redux';
+import App from './components/App';
 
-import * as actions from "./actions"
+import * as actions from './actions';
 
-function mapStateToProps(state){
-  return { bmiValue: state.bmiCalc.bmiValue }
+function mapStateToProps(state) {
+  return { bmiValue: state.bmiCalc.bmiValue };
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators(actions, dispatch)
+  return bindActionCreators(actions, dispatch);
 }
 
 export default function createContainer(store) {
-  let ConnectedApp = connect(mapStateToProps, mapDispatchToProps)(App)
+  let ConnectedApp = connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(App);
   return (
     <Provider store={store}>
       <div>
         <ConnectedApp />
-        <DevTools />
       </div>
     </Provider>
-  )
+  );
 }
--- src/store.js
+++ src/store.js
@@ -1,44 +1,7 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore } from 'redux';
 import rootReducer from './reducers.js';
-import DevTools from './containers/DevTools';
-import { persistState } from 'redux-devtools';
 
-const enhancer = compose(
-  // Middleware you want to use in development:
-  // applyMiddleware(d1, d2, d3),
-  // Required! Enable Redux DevTools with the monitors you chose
-  DevTools.instrument(),
-  // Optional. Lets you write ?debug_session=<key> in address bar to persist debug sessions
-  persistState(getDebugSessionKey())
-);
-
-function getDebugSessionKey() {
-  // You can write custom logic here!
-  // By default we try to read the key from ?debug_session=<key> in the address bar
-  const matches = window.location.href.match(/[?&]debug_session=([^&#]+)\b/);
-  return (matches && matches.length > 0)? matches[1] : null;
-}
-
-export default function setupStore(){
-  const initialState = {
-  }
-  let store
-  if (process.env.NODE_ENV === 'production') {
-    store = createStore(
-      rootReducer,
-      initialState)
-  }
-  else {
-    store = createStore(
-      rootReducer,
-      initialState,
-      enhancer
-    )
-  }
-  if (module.hot) {
-    module.hot.accept('./reducers', () =>
-      store.replaceReducer(require('./reducers')/*.default if you use Babel 6+ */)
-    );
-  }
-  return store;
+export default function setupStore() {
+  const initialState = {};
+  return createStore(rootReducer, initialState);
 }
